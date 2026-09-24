fce73d9b63a8372de7ae23e57325d54aef65a4b3
Mohamed-Akshan <mohamedakshan.dev@gmail.com>
2026-09-24 16:18:44 +0530

    Fix Vercel frontend and backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,6 +53,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://scholar-ai-frontend-eight.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
